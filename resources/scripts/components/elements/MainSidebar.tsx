2cbbcdfab734872f1f0e63ddca7d2ee19bbf6a16
GraphStats <no.replybot5455@gmail.com>
2026-01-17 20:59:04 +0100

    fix

--- resources/scripts/components/elements/MainSidebar.tsx
+++ resources/scripts/components/elements/MainSidebar.tsx
@@ -13,32 +13,9 @@ const MainSidebar: any = styled.nav`
     background: rgba(0, 0, 0, 0.6);
     border: 1px solid rgba(255, 255, 255, 0.08);
 
-    & > .openpanel-subnav-routes-wrapper {
-        display: flex;
-        flex-direction: column;
-        font-size: 14px;
-
-        & > a,
-        & > div {
-            display: flex;
-            position: relative;
-            padding: 12px 16px;
-            gap: 12px;
-            font-weight: 600;
-            min-height: 48px;
-            -webkit-tap-highlight-color: transparent;
-            user-select: none;
-            user-drag: none;
-            -ms-user-drag: none;
-            -moz-user-drag: none;
-            -webkit-user-drag: none;
-            transition: 200ms all ease-in-out;
-
-            &.active {
-                color: #fa4e49;
-                fill: #fa4e49;
-            }
-        }
+    .active {
+        color: #fa4e49;
+        fill: #fa4e49;
     }
 `;
 
