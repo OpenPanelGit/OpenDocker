7fc72de447af820fc21e98929cada7df8dd4ce09
GraphStats <no.replybot5455@gmail.com>
2026-01-18 13:56:32 +0100

    fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,7 +63,6 @@ class Kernel extends HttpKernel
             VerifyCsrfToken::class,
             SubstituteBindings::class,
             LanguageMiddleware::class,
-            \App\Http\Middleware\CheckSuspended::class,
         ],
         'api' => [
             EncryptCookies::class,
